2bdb57120303d6a65e726d94080f0ccc30ed6005
yuta519 <yuta519.ka23tf@docomo.ne.jp>
2021-11-15 14:19:15 +0900

    add time sleep before fetching exempting urls

--- crawl_zia_help.py
+++ crawl_zia_help.py
@@ -23,25 +23,19 @@ def crawl_certificate_pinning_ssl_inspection(driver: webdriver) -> None:
     #     # locate_with(By.CLASS_NAME, "optanon-alert-box-bottom-top")
     # ).click()
 
-    row_group = driver.find_elements(locate_with(By.CLASS_NAME, "article-page"))
+    time.sleep(2)
+    # row_group = driver.find_elements(locate_with(By.CLASS_NAME, "z-custom-grid"))
+    row_group = driver.find_elements(locate_with(By.CLASS_NAME, "ag-body-viewport"))
+
     for row in row_group:
         print(row.text)
 
-    # element = driver.find_element(
-    #     locate_with(By.XPATH, '//*[@id="ag-4"]/span[2]/div[3]')
-    # )
-    # print(element.click())
-    # # driver.execute_script("arguments[0].click();", element)
-    # row_group = driver.find_elements(locate_with(By.CLASS_NAME, "article-page"))
-    # for row in row_group:
-    #     print(row.text)
-
     time.sleep(3)
 
-    # driver.get("https://help.zscaler.com/zia/certificate-pinning-and-ssl-inspection")
-    # row_group = driver.find_elements(locate_with(By.CLASS_NAME, "article-page"))
-    # for row in row_group:
-    #     print(row.text)
+    test = driver.find_elements(locate_with(By.CLASS_NAME, "ag-paging-button"))
+    [print(row) for row in test]
+
+    # ag-paging-button
 
 
 if __name__ == "__main__":
